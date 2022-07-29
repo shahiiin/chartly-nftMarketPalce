5ea3586ed5dd81c703d2fce8975e595e76cef6c0
shahin <shahin.shahi1986@gmail.com>
2022-07-29 12:36:00 +0300

    make inline style

--- components/nav.js
+++ components/nav.js
@@ -8,17 +8,7 @@ import { useEffect, useState } from "react";
 import connect from "./utils/auth";
 import Logo from '../assets/cultchain.svg'
 
-const style = {
-  wrapper: `bg-[#04111d] w-screen px-[1.2rem] py-[0.8rem] flex `,
-  logoContainer: `flex items-center cursor-pointer`,
-  logoText: ` ml-[0.8rem] text-white font-semibold text-2xl`,
-  searchBar: `flex flex-1 mx-[0.8rem] w-max-[520px] items-center bg-[#363840] rounded-[0.8rem] hover:bg-[#4c505c]`,
-  searchIcon: `text-[#8a939b] mx-3 font-bold text-lg`,
-  searchInput: `h-[2.6rem] w-full border-0 bg-transparent outline-0 ring-0 px-2 pl-0 text-[#e6e8eb] placeholder:text-[#8a939b]`,
-  headerItems: ` flex items-center justify-end`,
-  headerItem: `text-white px-4 font-bold text-[#c8cacd] hover:text-white cursor-pointer`,
-  headerIcon: `text-[#8a939b] text-3xl font-black px-4 hover:text-white cursor-pointer`,
-};
+
 
 export default function Nav() {
   const [account, setAccount] = useState();
@@ -33,47 +23,47 @@ export default function Nav() {
   }
 
   return (
-    <div className={style.wrapper}>
+    <div className="bg-[#04111d] w-screen px-[1.2rem] py-[0.8rem] flex">
       <Link href="/">
-        <div className={style.logoContainer}>
+        <div className="flex items-center cursor-pointer">
           <Image
             src={Logo}
             alt="cultchain logo"
             height={40}
             width={40}
           />
-          <div className={style.logoText}>Cultchain</div>
+          <div className="ml-[0.8rem] text-white font-semibold text-2xl">Cultchain</div>
         </div>
       </Link>
-      <div className={style.searchBar}>
-        <div className={style.searchIcon}>
+      <div className="flex flex-1 mx-[0.8rem] w-max-[520px] items-center bg-[#363840] rounded-[0.8rem] hover:bg-[#4c505c]">
+        <div className="text-[#8a939b] mx-3 font-bold text-lg">
           <AiOutlineSearch />
         </div>
         <input
-          className={style.searchInput}
+          className="h-[2.6rem] w-full border-0 bg-transparent outline-0 ring-0 px-2 pl-0 text-[#e6e8eb] placeholder:text-[#8a939b]"
           placeholder="Search Collections, NFTs, and Artists"
         />
       </div>
-      <div className={style.headerItems}>
+      <div className="flex items-center justify-end">
         <Link href="/explore">
-          <div className={style.headerItem}>Explore</div>
+          <div className="text-white px-4 font-bold text-[#c8cacd] hover:text-white cursor-pointer">Explore</div>
         </Link>
         <Link href="/create-nft">
-          <div className={style.headerItem}>Mint</div>
+          <div className="text-white px-4 font-bold text-[#c8cacd] hover:text-white cursor-pointer">Mint</div>
         </Link>
         <Link href="/my-nfts">
-          <div className={style.headerItem}>My NFTs</div>
+          <div className="text-white px-4 font-bold text-[#c8cacd] hover:text-white cursor-pointer">My NFTs</div>
         </Link>
         <Link href="/dashboard">
-          <div className={style.headerItem}>Dashboard</div>
+          <div className="text-white px-4 font-bold text-[#c8cacd] hover:text-white cursor-pointer">Dashboard</div>
         </Link>
         <Link href="/create-collections">
-          <div className={style.headerItem}>Collections</div>
+          <div className="text-white px-4 font-bold text-[#c8cacd] hover:text-white cursor-pointer">Collections</div>
         </Link>
-        <div className={style.headerIcon}>
+        <div className="text-[#8a939b] text-3xl font-black px-4 hover:text-white cursor-pointer">
           <CgProfile />
         </div>
-        <div className={style.headerIcon}>
+        <div className="text-[#8a939b] text-3xl font-black px-4 hover:text-white cursor-pointer">
           <MdOutlineAccountBalanceWallet />
         </div>
       </div>
